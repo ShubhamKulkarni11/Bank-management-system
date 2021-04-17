59780745def5d1e6fb1b0bb6a784b9d33da39443
ShubhamKulkarni11 <79390070+ShubhamKulkarni11@users.noreply.github.com>
2021-04-17 21:25:26 +0530

    Update settings.py

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
